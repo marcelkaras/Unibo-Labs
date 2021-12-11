3cb722b41b9944197b7c5fe8e01959c998e1c5a0
marcelkaras <43673205+marcelkaras@users.noreply.github.com>
2021-12-11 15:19:40 +0100

    results

--- Lab4-RT/Labreport/plot.py
+++ Lab4-RT/Labreport/plot.py
@@ -65,13 +65,26 @@ print('R0_3 = ', params3[0], r'\pm', err3[0])
 print('b3 = ', params3[1], r'\pm', err3[1])
 
 #exponential fit for germanium
-params4, cov4 = curve_fit(expo,T[100:],R1[100:],p0=[1,1,1])
+params4, cov4 = curve_fit(expo,T[120:],R1[120:],p0=[1,1,1])
 err4 = np.sqrt(np.diag(cov4))
+print('Size of T:',(T.size-110)*1000/T.size)
+print('Size of T:',(T.size-92)*1000/T.size)
+print('T(120)',T[120])
 
 print('\nExp-Ge:')
 print('A = ', params4[0], r'\pm', err4[0])
 print('B = ', params4[1], r'\pm', err4[1])
+print('B*2k= ',params4[1]*2*const.k/const.e, r'\pm', err4[1]*2*const.k/const.e)
+print('k',const.Boltzmann/const.e)
 print('C = ', params4[2], r'\pm', err4[2])
+
+#logarithmic linear fit for germanium
+params5, cov5 = curve_fit(metals,np.log(T[:92]),np.log(R1[:92]),p0=[1,1])
+err5 = np.sqrt(np.diag(cov5))
+print('\nLog-lin-Ge:')
+print('A = ', params5[0], r'\pm', err5[0])
+print('B = ', params5[1], r'\pm', err5[1])
+
 
 #Plot of 
 
@@ -119,8 +132,8 @@ ax = fig.add_axes([0.1, 0.13, 0.8, 0.83])
 
 # Plot und Labels/ Legende
 ax.plot(T, R1, 'k.', label='Germanium')
-ax.plot(TT, metals(TT,*params1), 'b-', label='Linear fit')
-ax.plot(TT[500:], expo(TT[500:],*params4), 'r-', label='Exponential fit')
+ax.plot(TT[:420], metals(TT[:420],*params1), 'g-', label='Linear fit')
+ax.plot(T[120:], expo(T[120:],*params4), 'r-', label='Exponential fit')
 #ax.plot(TT, gaussian(TT, *params), '-', color='#EC0000', label='Gaussian fit') #label=r'Fit $B(z) = az^4 + bz^3 + cz^2 + dz + e$')
 ax.set_xlabel(r'$T \:/\: \si{\kelvin}$')
 ax.set_ylabel(r'$R \:/\: \si{\ohm}$')
@@ -150,7 +163,7 @@ ax = fig.add_axes([0.1, 0.13, 0.8, 0.83])
 
 # Plot und Labels/ Legende
 ax.plot(T, R2, 'k.', label='Copper')
-ax.plot(TT, metals(TT,*params2), 'b-', label='Linear fit')
+ax.plot(TT, metals(TT,*params2), 'r-', label='Linear fit')
 #ax.plot(TT, gaussian(TT, *params), '-', color='#EC0000', label='Gaussian fit') #label=r'Fit $B(z) = az^4 + bz^3 + cz^2 + dz + e$')
 ax.set_xlabel(r'$T \:/\: \si{\kelvin}$')
 ax.set_ylabel(r'$R \:/\: \si{\ohm}$')
@@ -180,7 +193,7 @@ ax = fig.add_axes([0.1, 0.13, 0.8, 0.83])
 
 # Plot und Labels/ Legende
 ax.plot(T, R3, 'k.', label='Nickel')
-ax.plot(TT, metals(TT,*params3), 'b-', label='Linear fit')
+ax.plot(TT, metals(TT,*params3), 'r-', label='Linear fit')
 #ax.plot(TT, gaussian(TT, *params), '-', color='#EC0000', label='Gaussian fit') #label=r'Fit $B(z) = az^4 + bz^3 + cz^2 + dz + e$')
 ax.set_xlabel(r'$T \:/\: \si{\kelvin}$')
 ax.set_ylabel(r'$R \:/\: \si{\ohm}$')
@@ -200,3 +213,51 @@ ax.tick_params(which='minor', direction='in', length=4, width=0.3)
 plt.savefig('plots/R3.pdf',bbox_inches = "tight")
 
 #-------------------------------------------------------------------------------------------------------------
+#double logarithmic plot for germanium low temperature
+#-------------------------------------------------------------------------------------------------------------
+#plotten germanium
+
+fig = plt.figure()
+ax = fig.add_axes([0.1, 0.13, 0.8, 0.83])
+# for axis in ['top','bottom','left','right']:
+#   ax.spines[axis].set_linewidth(0.3)
+
+# Plot und Labels/ Legende
+ax.plot(np.log(T), np.log(R1), 'k.', label='Germanium')
+ax.plot(np.log(T[:92]), metals(np.log(T[:92]),*params5), 'r-', label='Linear fit')
+#ax.plot(TT, gaussian(TT, *params), '-', color='#EC0000', label='Gaussian fit') #label=r'Fit $B(z) = az^4 + bz^3 + cz^2 + dz + e$')
+ax.set_xlabel(r'$ln(T) \:/\: a.u.$')
+ax.set_ylabel(r'$ln(R) \:/\: a.u.$')
+leg1 = ax.legend(loc='best', fancybox=False, prop={'size':16}, edgecolor='k')
+leg1.get_frame().set_linewidth(0.3)
+
+# Einstellung der Achsen
+#ax.set_xlim(T[0],T[T.size-1])
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax.tick_params(axis='both', direction='in')
+ax.tick_params(which='major', direction='in', length=7, width=0.3)
+ax.tick_params(which='minor', direction='in', length=4, width=0.3)
+
+# in matplotlibrc leider (noch) nicht möglich
+#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('plots/R1-log.pdf',bbox_inches = "tight")
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Discussion
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+#####Second part of the exoeriment
+#-------------------------------------------------------------------------------------------------------------
+
